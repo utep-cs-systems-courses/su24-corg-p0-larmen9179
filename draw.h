313adf652764811bafc46b727f6c0d1b9539eb4f
logan <lo.j.armendariz@gmail.com>
2024-06-15 20:20:53 -0600

    added upright char print method

--- draw.h
+++ draw.h
@@ -5,7 +5,13 @@ void print_triangle(int startCol, int size);
 void print_square(int startCol, int size);
 void print_arrow(int startCol, int squareSize, int triangleSize);
 void print_char_5x7(char c);
+void print_char_up_5x7(char c);
+
+//functions for different bitmap font
+void print_char_11x16(short c);
+void print_char_up_11x16(short c);
 
 extern const unsigned char font_5x7[][5];
+extern const unsigned short font_11x16[][11];
 
 #endif
--- draw_chars.c
+++ draw_chars.c
@@ -5,8 +5,8 @@ void print_char_5x7(char c)
 {
   c -= 0x20;
   for (char col = 0; col < 5; col++) {
+    unsigned short rowBits = font_5x7[c][col];
     for (char row = 0; row < 7; row++) {
-      unsigned short rowBits = font_5x7[c][col];
       unsigned short colMask = 1 << (6-row); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
@@ -14,3 +14,49 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+void print_char_up_5x7(char c)
+{
+  c -= 0x20;
+  for (char row = 7; row >= 0; row--){
+    unsigned short colMask = 1 << (6 - row);
+    for (char col = 0; col < 5; col++){
+      unsigned short rowBits = font_5x7[c][col];
+      putchar( (rowBits & colMask) ? '*': ' ');
+    }
+    putchar('\n');
+  }
+      
+      
+}
+
+void print_char_11x16(short c)
+{
+  c -= 0x20;
+  for (char col = 0; col < 5; col++) {
+    unsigned short rowBits = font_5x7[c][col];
+    for (char row = 0; row < 7; row++) {
+      unsigned short colMask = 1 << (6-row); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
+void print_char_up_11x16(short c)
+{
+  c -= 0x20;
+  for (char row = 7; row >= 0; row--){
+    unsigned short colMask = 1 << (6 - row);
+    for (char col = 0; col < 5; col++){
+      unsigned short rowBits = font_5x7[c][col];
+      putchar( (rowBits & colMask) ? '*': ' ');
+    }
+    putchar('\n');
+  } 
+}
+
+
+
+  
